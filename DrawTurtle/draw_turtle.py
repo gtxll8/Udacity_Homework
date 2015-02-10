e1297cb833caf31fee9f049fde245f5d3c3733ac
gtxll8 <george@chessborg.com>
2015-02-10 22:58:51 +0000

    new files

--- DrawTurtle/draw_turtle.py
+++ DrawTurtle/draw_turtle.py
@@ -1,1 +1,12 @@
 __author__ = 'g'
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+    window.exitonclick()
+
+draw_square()
