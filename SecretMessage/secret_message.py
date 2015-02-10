ebbf10160745e91baf53002f2b10391d4a3fa504
gtxll8 <george@chessborg.com>
2015-02-10 21:10:48 +0000

    new files

--- SecretMessage/secret_message.py
+++ SecretMessage/secret_message.py
@@ -1,9 +1,11 @@
 __author__ = 'gcalinescu'
 import os
+import shutil
 def rename_files():
     #(1) get teh file names from a folder
     target_path = os.getcwd()
-    file_list = os.listdir(target_path + "\prank")
+    #(!) For windows use backslash like 'this '\prank'
+    file_list = os.listdir(target_path + "/prank")
     #print=(file_list)
     print("Current working directory is: "+target_path)
     os.chdir(target_path+"\prank")
@@ -17,13 +19,40 @@ def rename_files():
 def create_message():
     #(1) define the secret message
     secret_message = "HELLO THERE"
-    #(2) clear directory prank of any old files
+    #(2) clear directoryprank of any old files
     target_path = os.getcwd()
-    os.chdir(target_path+"\prank")
-    for files_to_remove in os.listdir(target_path + "\prank"):
+    os.chdir(target_path+"/prank")
+    for files_to_remove in os.listdir(target_path + "/prank"):
         print(files_to_remove)
         os.remove(files_to_remove)
+    #(3) get the list of files from alphabet directory + sort
+    alphabet_files = os.listdir(target_path+"/alphabet")
+    alphabet_files.sort()
+    file_name_letter = alphabet_files[1]
+    print(file_name_letter)
+    #(4) translate secret message to numbers, using 'ord()'
+    #exception is the space trap the '-64' and replace it with 'madrid.jpg'
+    #   print alphabet_files
+    alphabet_folder = target_path+"/alphabet/"
+    prank_folder = target_path+"/prank/"
+    index_counter = 1
+    for letter in secret_message:
+        letter_number = ord(letter.lower()) - 96
+        if letter_number == -64:
+            letter_file_name = 'madrid.jpg'
+            print letter_file_name
+            shutil.copyfile(alphabet_folder+letter_file_name, prank_folder+(chr(96 + index_counter) + letter_file_name))
+        else:
+            print(alphabet_files[letter_number - 1])
+            print(letter)
+            print(letter_number)
+            letter_file_name = alphabet_files[letter_number - 1]
+            shutil.copyfile(alphabet_folder+letter_file_name, prank_folder+(chr(96 + index_counter) + letter_file_name))
+        index_counter += 1
 
+
+
+        #print numerical_message
 
 create_message()
 
