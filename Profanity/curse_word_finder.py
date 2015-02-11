f05e094c1968d83935c76f2b74b5fa29f04d25d2
gtxll8 <george@chessborg.com>
2015-02-11 21:46:22 +0000

    new files

--- Profanity/curse_word_finder.py
+++ Profanity/curse_word_finder.py
@@ -3,9 +3,24 @@ import os
 import urllib
 
 def read_text():
-    path = os.getcwd
-    quotes = open(path+"movie_quotes")
-    print path
+    path = os.getcwd()
+    quotes = open(path+"/movie_quotes")
     contents_of_file = quotes.read()
     print (contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert !")
+    elif "false" in output:
+        print("No profanities !")
+    else:
+        print("Could not connect !")
+
+
+
+read_text()
