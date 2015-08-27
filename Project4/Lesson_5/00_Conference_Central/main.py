2692a8c0ab9f4068cfb4e47886975b0e22f8ba2f
gtxll8 <george@chessborg.com>
2015-08-27 17:21:23 +0100

    Added homework

--- Project4/Lesson_5/00_Conference_Central/main.py
+++ Project4/Lesson_5/00_Conference_Central/main.py
@@ -15,6 +15,7 @@ __author__ = 'wesc+api@google.com (Wesley Chun)'
 import webapp2
 from google.appengine.api import app_identity
 from google.appengine.api import mail
+from google.appengine.api import memcache
 from conference import ConferenceApi
 
 class SetAnnouncementHandler(webapp2.RequestHandler):
